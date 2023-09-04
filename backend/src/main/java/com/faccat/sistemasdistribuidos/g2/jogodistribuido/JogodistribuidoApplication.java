fa33309b96835662e7af98fc65dc4ae94f010e21
Tim Morgenstern <tim@infisc.com.br>
2023-09-04 20:16:50 -0300

    backend

---
+++ backend/src/main/java/com/faccat/sistemasdistribuidos/g2/jogodistribuido/JogodistribuidoApplication.java
@@ -0,0 +1,13 @@
+package com.faccat.sistemasdistribuidos.g2.jogodistribuido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogodistribuidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogodistribuidoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/faccat/sistemasdistribuidos/g2/jogodistribuido/JogodistribuidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faccat.sistemasdistribuidos.g2.jogodistribuido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogodistribuidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
